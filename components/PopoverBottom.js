70269838dcdeb91d66a361d257087da4955a2bcc
Wang Shaolong <EmilWang@GRAPECITY.NET>
2017-07-30 15:36:27 +0800

    first commit

---
+++ components/PopoverBottom.js
@@ -0,0 +1,33 @@
+import  React,{Component} from 'react';
+import {Popover} from 'react-bootstrap';
+
+export default class PopoverBottom extends Component{
+  constructor(props){
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleAdd(){
+    this.props.onAddResource(this.input.value);
+    this.input.value = "";
+  }
+
+  handleClose(){
+    this.props.onClose()
+  }
+
+  render(){
+    return (
+      <Popover {...this.props} id="popover-positioned-bottom" className="popoverBottom"> 
+       <strong>(separate multiple resources name with commas)</strong>
+       <input 
+         ref={(c)=>{this.input=c}} 
+         className="resourceInput"/>
+       <div style={{margin:"5px"}}>
+         <button onClick={this.handleAdd}>Add Resources</button>
+         <button onClick={this.handleClose}>Close</button>
+       </div>
+      </Popover>);
+  }
+}
